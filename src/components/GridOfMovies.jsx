c18fc7a3157db3bfc06d68eddf96f3dd0d7d9c22
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-05-16 10:28:08 -0500

    finish the pagination for the grid of movies

--- src/components/GridOfMovies.jsx
+++ src/components/GridOfMovies.jsx
@@ -1,7 +1,13 @@
-import { Grid, Typography, IconButton, Box } from "@mui/material";
+import { Grid, Typography, Button, IconButton, Box } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 
-const GridOfMovies = ({ movies }) => {
+const GridOfMovies = ({
+  movies,
+  onNextPage,
+  onPrevPage,
+  currentPage,
+  totalPages,
+}) => {
   const getMovieImageUrl = (movie) => {
     if (!movie.poster_path) {
       return null;
@@ -10,76 +16,100 @@ const GridOfMovies = ({ movies }) => {
   };
 
   return (
-    <Grid container spacing={1}>
-      {movies.map((movie, index) => (
-        <Grid item key={index} xs={6} sm={4} md={3} lg={2}>
-          <Box
-            sx={{
-              borderRadius: "0 0 5px 5px",
-              overflow: "hidden",
-              display: "flex",
-              flexDirection: "column",
-              height: "100%",
-              backgroundColor:"red"
-            }}
-          >
+    <>
+      <Grid container spacing={1}>
+        {movies.map((movie, index) => (
+          <Grid item key={index} xs={6} sm={4} md={3} lg={2}>
             <Box
               sx={{
-                height: "60vh",
-                backgroundImage: `url(${getMovieImageUrl(movie)})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center top",
-                display: "flex",
-                alignItems: "flex-end",
-              }}
-              onClick={() => console.log("Hola")}
-
-            ></Box>
-            <Box
-              sx={{
-                backgroundColor: "#f4f4f4",
-                width: "100%",
+                borderRadius: "0 0 5px 5px",
                 overflow: "hidden",
-                maxHeight: "50px", 
+                display: "flex",
+                flexDirection: "column",
+                height: "100%",
+                backgroundColor: "red",
               }}
             >
-              <Typography
-                variant="h6"
+              <Box
                 sx={{
-                  fontWeight: "bold",
-                  textAlign: "center",
-                  color: "blue",
-                  padding: "10px",
+                  height: "60vh",
+                  backgroundImage: `url(${getMovieImageUrl(movie)})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center top",
+                  display: "flex",
+                  alignItems: "flex-end",
+                }}
+                onClick={() => console.log("Hola")}
+              ></Box>
+              <Box
+                sx={{
+                  backgroundColor: "#f4f4f4",
+                  width: "100%",
                   overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
+                  maxHeight: "50px",
                 }}
               >
-                {movie.title}
-              </Typography>
-            </Box>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    color: "blue",
+                    padding: "10px",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  {movie.title}
+                </Typography>
+              </Box>
 
-            <IconButton
-              sx={{
-                backgroundColor: "blue",
-                color: "#fff",
-                borderRadius: "0",
-                flexGrow: 1,
-                maxHeight: "50px",
-                "&:hover": {
-                  backgroundColor: "red",
-                  "& svg": {
-                    color: "blue", 
+              <IconButton
+                sx={{
+                  backgroundColor: "blue",
+                  color: "#fff",
+                  borderRadius: "0",
+                  flexGrow: 1,
+                  maxHeight: "50px",
+                  "&:hover": {
+                    backgroundColor: "red",
+                    "& svg": {
+                      color: "blue",
+                    },
                   },
-                },
-              }}
-            >
-              <VisibilityIcon />
-            </IconButton>
-          </Box>
-        </Grid>
-      ))}
-    </Grid>
+                }}
+              >
+                <VisibilityIcon />
+              </IconButton>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+      <Box display="flex" justifyContent="center" marginTop="5vh">
+        <Button
+          variant="contained"
+          onClick={onPrevPage}
+          disabled={currentPage === 1}
+          style={{ marginRight: "10px" }}
+        >
+          Previous Page
+        </Button>
+        <Typography
+          variant="body1"
+          style={{ margin: "0 10px", fontWeight: "bold" }}
+        >
+          Page {currentPage} of {totalPages}
+        </Typography>
+        <Button
+          variant="contained"
+          onClick={onNextPage}
+          disabled={currentPage === totalPages}
+        >
+          Next Page
+        </Button>
+      </Box>
+    </>
   );
 };
 
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import MovieCarousel from "./MovieCarousel";
 import MovieSlide from "./MovieSlide";
 import QuickDisplayHomeMovies from "./QuickDisplayHomeMovies";
 import GridOfMovies from "./GridOfMovies";
+import Pagination from "./Pagination";
 
 
-export {MovieCarousel, MovieSlide, QuickDisplayHomeMovies, GridOfMovies}
+export {MovieCarousel, MovieSlide, QuickDisplayHomeMovies, GridOfMovies, Pagination}
