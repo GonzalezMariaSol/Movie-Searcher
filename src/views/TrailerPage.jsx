69e8074dbf75e0f2573b3c8a58bb6b98e6ead9ce
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-05-23 11:20:04 -0500

    added exit btn for movie trailer

--- src/views/TrailerPage.jsx
+++ src/views/TrailerPage.jsx
@@ -1,16 +1,41 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useMovie } from "../hooks";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+// NAVEGACION
+import { useNavigate } from "react-router-dom";
+
 
 const TrailerPage = () => {
   const { trailerMovieInfo, getMovieForTrailer } = useMovie(); //me traigo la funcion para hacer que traiga la URL de loS videoS, y donde lo guardamos
 
-  const { videoId } = useParams(); //me trae el id de la pelicula seleccionada 
+  const { videoId } = useParams(); //me trae el id de la pelicula seleccionada
+
+  const navigate = useNavigate(); // inicializamos funcion navigate
 
   useEffect(() => {
     getMovieForTrailer(videoId); //inicializo la funcion para poder obtener la URL
   }, []);
+
+  const NeonTextStyle = {
+    color: "#ff6a6a",
+    textShadow: `
+      0 0 5px red,
+      0 0 10px red,
+      0 0 20px red,
+      0 0 30px red,
+      0 0 40px red,
+      0 0 50px red,
+      0 0 60px red
+    `,
+  };
+  const NeonIconStyle = {
+    color: "#ff6a6a",
+    filter:
+      "drop-shadow(0 0 5px red) drop-shadow(0 0 10px red) drop-shadow(0 0 15px red) drop-shadow(0 0 20px red)",
+  };
 
   return (
     <Box
@@ -19,15 +44,17 @@ const TrailerPage = () => {
         justifyContent: "center",
         alignItems: "center",
         height: {
-          xs: "50vh", 
-          sm: "70vh", 
-          md: "80vh", 
-          lg: "90vh", 
+          xs: "50vh",
+          sm: "70vh",
+          md: "80vh",
+          lg: "90vh",
         },
         backgroundColor: "black",
         padding: "20px",
         position: "relative",
         overflow: "hidden",
+        flexWrap: "wrap",
+        minHeight: "100%",
       }}
     >
       {trailerMovieInfo && (
@@ -35,7 +62,7 @@ const TrailerPage = () => {
           sx={{
             position: "relative",
             width: "80%",
-            paddingBottom: "45%", 
+            paddingBottom: "45%",
             height: 0,
           }}
         >
@@ -54,8 +81,40 @@ const TrailerPage = () => {
           ></iframe>
         </Box>
       )}
+
+      <Button
+      onClick={() => navigate(`/detail/${videoId}`)}
+        sx={{
+          margin: "5vh",
+          backgroundColor: "black",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "1vw",
+          borderRadius: "1.5vw",
+          border: "0.2vw solid #ff6a6a",
+          boxShadow: `
+          0 0 0.4vw red,
+          0 0 0.8vw red,
+          0 0 1.2vw red,
+          0 0 1.6vw red
+        `,
+          position: "relative",
+        }}
+      >
+        <p
+          style={{
+            display: "flex",
+            alignItems: "center",
+            margin: 0,
+            ...NeonTextStyle,
+          }}
+        >
+          EXIT <DirectionsRunIcon sx={{ ...NeonIconStyle }} />
+          <ArrowForwardIcon sx={{ ...NeonIconStyle }} />
+        </p>
+      </Button>
     </Box>
   );
-  
 };
 export default TrailerPage;
